8c73a0578a8ed53edcfd51efc8fc3e8d81bc3cd4
Tim-Jonas Wechler <code.wechlertimjonas@icloud.com>
2026-07-01 17:31:32 +0200

    feat(profile-dsl): add compiler execution plan skeleton
    Refs #93

---
+++ src-tauri/src/profile_dsl/execution_plan/mod.rs
@@ -0,0 +1,38 @@
+use serde::{Deserialize, Serialize};
+
+use crate::profile_dsl::documents::{PostingDetailStep, PostingDiscoveryStep, SourceOverrides};
+use crate::source::documents::SourceConfig;
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SourceExecutionPlan {
+    pub source: ExecutionPlanSource,
+    pub selected_access_path: ExecutionPlanAccessPath,
+    pub source_config: SourceConfig,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub source_overrides: Option<SourceOverrides>,
+    pub posting_discovery: PostingDiscoveryStep,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub posting_detail: Option<PostingDetailStep>,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ExecutionPlanSource {
+    pub key: String,
+    pub name: String,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum ExecutionPlanAccessPath {
+    #[serde(rename_all = "camelCase")]
+    ProfileAccessPath {
+        profile_key: String,
+        profile_name: String,
+        path_key: String,
+        path_name: String,
+    },
+    #[serde(rename_all = "camelCase")]
+    SourceOwnedAccessPath { key: String, name: String },
+}
